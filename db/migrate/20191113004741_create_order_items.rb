25e692d02f8a9a6234913f9c995b1d57331fa852
Alex Petersen <lexsp25@gmail.com>
2019-11-26 13:33:43 -0500

    Removed price from order_items table via the 20191113004741_create_order_items migration

--- db/migrate/20191113004741_create_order_items.rb
+++ db/migrate/20191113004741_create_order_items.rb
@@ -2,7 +2,6 @@ class CreateOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_items do |t|
       t.integer :quantity, default:0
-      t.decimal :price, precision: 15, scale: 2, null:false
 
       t.timestamps
     end
