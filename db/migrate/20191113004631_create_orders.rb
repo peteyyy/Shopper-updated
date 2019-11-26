c36b0a1e68184c7dc42c33164c33535f83e251fd
Alex Petersen <lexsp25@gmail.com>
2019-11-26 13:36:22 -0500

    Added user_id to the order table via 20191113004741_create_order_items migration

--- db/migrate/20191113004631_create_orders.rb
+++ db/migrate/20191113004631_create_orders.rb
@@ -5,6 +5,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
       t.string :last_name, null:false
       t.decimal :subtotal, precision: 15, scale: 2
       t.string :token
+      t.integer :user_id
 
       t.timestamps
     end
