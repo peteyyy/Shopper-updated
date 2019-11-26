f8ab9d6257acb7a4d3028d6358f76e5a848b6a75
Alex Petersen <lexsp25@gmail.com>
2019-11-26 13:35:01 -0500

    Added product_id to the order_items table via 20191113004741_create_order_items migration

--- db/migrate/20191113004741_create_order_items.rb
+++ db/migrate/20191113004741_create_order_items.rb
@@ -2,6 +2,7 @@ class CreateOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_items do |t|
       t.integer :quantity, default:0
+      t.integer :product_id
 
       t.timestamps
     end
