cba0d0a89c8b1e6dfca9665e932d7139f480a580
Alex Petersen <lexsp25@gmail.com>
2019-11-26 13:55:16 -0500

    Removed code from OrderItemsController regarding shopping carts. All 3 methods should be empty.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,16 +1,11 @@
 class OrderItemsController < ApplicationController
   def index
-    @items = current_cart.items
   end
 
   def create
-    current_cart.add_item(params[:product_id], params[:quantity])
-    redirect_to cart_path
   end
 
   def destroy
-    current_cart.remove_item(id: params[:id])
-    redirect_to cart_path
   end
 
 end
