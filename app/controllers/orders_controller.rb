0b154897519f4280843d73cf9f28ef9ce556f98e
Alex Petersen <lexsp25@gmail.com>
2019-11-26 14:06:42 -0500

    Starting in the products/index.html.erb file, the form should now send to the additem_path (which was previously added to your routes). In your OrdersController, the new and create method have been cleared out. A new add_item method is created which will get information from the params from the form, then create a new item and place it in the current order. A current_order helper method was created as well.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,35 @@
 class OrdersController < ApplicationController
 
   def new
-    @order = current_cart.order
   end
 
   def create
-    @order = current_cart.order
-    if @order.update_attributes(order_params.merge(status: 'open'))
-      session[:cart_token] = nil
-      redirect_to root_path
-    else
-      render :new
-    end
+  end
+
+   def add_item
+    product_id = params[:product_id]
+    quantity = params[:quantity]
+    product = Product.find(product_id)
+    order_item = current_order.items.find_or_create_by(product_id: product_id, quantity: quantity, order_id: current_order.id)
+    redirect_to cart_path
   end
 
   private
 
+
+  helper_method :current_order
+  def current_order
+    current_user.orders.each do |order|
+      if order.status == "cart"
+        return @order = order
+      end
+    end
+    if !@order
+      return @order = Order.create(:order_params)
+    end
+  end
+
   def order_params
-    params.require(:order).permit(:first_name, :last_name)
+    params.require(:order).permit(:user_id, :status, :subtotal)
   end
 end
