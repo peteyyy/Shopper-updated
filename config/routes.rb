9f62ea25244c31e7a1e43482f4b9bc068be817c4
Alex Petersen <lexsp25@gmail.com>
2019-11-26 13:51:24 -0500

    Added resources :orders to routes file, which makes the two routes to orders#new and orders#create redundent. Comment those two out.

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
   resources :products, only: [:index, :new, :create] do
-  
+
     namespace :admin do
-    resources :products, only: [:new, :create, :show]
-  end
+      resources :products, only: [:new, :create, :show]
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 
   get '/cart', to: 'order_items#index'
   resources :order_items, path: 'cart/items'
+  resources :orders
+  post '/additem', to: 'orders#add_item'
 
-get '/cart/checkout', to: 'orders#new', as: :checkout
-patch '/cart/checkout', to: 'orders#create'
+# get '/cart/checkout', to: 'orders#new', as: :checkout
+# patch '/cart/checkout', to: 'orders#create'
 
 end
 
