5ca7df4b0cc02c84425e20978526acd34676833a
Alex Petersen <lexsp25@gmail.com>
2019-11-26 13:43:58 -0500

    Commented out everything regarding shopping_cart in the ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :current_cart
+#   before_action :current_cart
 
-  def current_cart
-    @current_cart ||= ShoppingCart.new(token: cart_token)
-  end
-  
-  helper_method :current_cart
-  
-  private
-#how does api work into this?
-    def cart_token
-      return @cart_token unless @cart_token.nil?
+#   def current_cart
+#     @current_cart ||= ShoppingCart.new(token: cart_token)
+#   end
 
-      session[:cart_token] ||= SecureRandom.hex(5)
-      @cart_token = session[:cart_token]
-    end
-  
+#   helper_method :current_cart
+
+#   private
+# #how does api work into this?
+#     def cart_token
+#       return @cart_token unless @cart_token.nil?
+
+#       session[:cart_token] ||= SecureRandom.hex(5)
+#       @cart_token = session[:cart_token]
+#     end
+
 end
