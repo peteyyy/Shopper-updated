71206f48eea960da5d06070033f2ef306a28490c
Alex Petersen <lexsp25@gmail.com>
2019-11-26 13:37:01 -0500

    Removed first and last name from orders table via 20191113004631_create_orders migration

--- db/migrate/20191113004631_create_orders.rb
+++ db/migrate/20191113004631_create_orders.rb
@@ -1,8 +1,6 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.string :first_name, null:false
-      t.string :last_name, null:false
       t.decimal :subtotal, precision: 15, scale: 2
       t.string :token
       t.integer :user_id
