f59602e4caee4594bd87fa8fcccb4acd5bfb2339
Alex Petersen <lexsp25@gmail.com>
2019-11-26 13:32:26 -0500

    Deleted 20191125225835_add_user_id_to_shopping_cart as we aren't using shopping cart anymore

--- db/migrate/20191118213041_add_shopping_cart_id_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddShoppingCartIdToOrders < ActiveRecord::Migration[5.2]
-  def change
-    add_column :orders, :shopping_cart_id, :integer
-  end
-end
--- db/migrate/20191125225835_add_user_id_to_shopping_cart.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToShoppingCart < ActiveRecord::Migration[5.2]
-  def change
-    add_column :shopping_cart, :user_id :integer
-  end
-end
